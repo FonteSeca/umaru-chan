cb2a768266c10798b0b143f1038b98bbefc9d72c
omg, PANTO!!! <omgpanto@gmail.com>
2017-08-11 22:07:01 -0300

    THE FIRST COMMITO
    First commito

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const Bot = new Discord.Client();
+
+Bot.on('ready', () => {
+    console.log(`[Startou] ${new Date()}`);
+});
+
+Bot.on('message', message => {
+    if(message.content = 'ping') {
+    	message.channel.send('PINGOU')
+    }
+});
+
+Bot.login(process.env.token);
