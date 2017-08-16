499d62755847198f1c441d75c8c017f48f1275b8
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-08-16 11:46:34 -0300

    Código Base

--- index.js
+++ index.js
@@ -5,10 +5,49 @@ Bot.on('ready', () => {
     console.log(`[Startou] ${new Date()}`);
 });
 
+music(Bot);
+
+let PREFIX = '+';
+
 Bot.on('message', message => {
+
+    const message = msg.content.trim();
+
+    // Verifica se a mensagem é um comando
+    if (message.toLowerCase().startsWith(PREFIX.toLowerCase())) {
+      // Get the command and suffix.
+      const command = message.substring(PREFIX.length).split(/[ \n]/)[0].toLowerCase().trim();
+      const suffix = message.substring(PREFIX.length + command.length).trim();
+
+switch (command) {
+case 'permission':
+return permission(msg, suffix);
+
+}
+
     if(message.content == 'ping') {
     	message.channel.send('PINGOU')
     }
+
+}
+
 });
+
+function basicembed(color,text) {
+    return {embed: {
+      color: color,
+      description: text
+      }};
+  }
+
+  function imageembed(color,image,text) {
+  return {embed: {
+    "color": color,
+    "description": text,
+    "image": {
+    "url": image}
+    }};
+}
+
 
 Bot.login(process.env.token);
