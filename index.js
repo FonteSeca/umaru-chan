77a4e8f7999f937e468a73e7d02e7e6eb4aec62d
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-08-18 21:41:33 -0300

    asd

--- index.js
+++ index.js
@@ -11,32 +11,27 @@ music(Bot);
 let PREFIX = '+';
 
 Bot.on('message', message => {
-<<<<<<< HEAD
     if(message.content === 'ping') {
-=======
 
     const message = msg.content.trim();
 
     // Verifica se a mensagem é um comando
     if (message.toLowerCase().startsWith(PREFIX.toLowerCase())) {
-      // Get the command and suffix.
-      const command = message.substring(PREFIX.length).split(/[ \n]/)[0].toLowerCase().trim();
-      const suffix = message.substring(PREFIX.length + command.length).trim();
+    	// Get the command and suffix.
+    	const command = message.substring(PREFIX.length).split(/[ \n]/)[0].toLowerCase().trim();
+    	const suffix = message.substring(PREFIX.length + command.length).trim();
 
-switch (command) {
-case 'permission':
-return permission(msg, suffix);
+		switch (command) {
+			case 'permission':
+			return permission(msg, suffix);
+		}
 
-}
+    	if(message.content == 'ping') {
+    		message.channel.send('PINGOU')
+    	}
+	}
 
-    if(message.content == 'ping') {
->>>>>>> 499d62755847198f1c441d75c8c017f48f1275b8
-    	message.channel.send('PINGOU')
-    }
-
-}
-
-});
+}});
 
 function basicembed(color,text) {
     return {embed: {
